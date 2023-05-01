8de7fdda8eeae9040002fa79a295ba58804e1173
ConsoleC137 <artemcelipotkin@gmail.com>
2023-05-01 23:30:59 +0700

    Добавлен код для перехода на страницу 'PageChangeMasters'

--- AutoServis/Pages/PageAdmin.xaml.cs
+++ AutoServis/Pages/PageAdmin.xaml.cs
@@ -34,7 +34,7 @@ namespace AutoServis.Pages
 
         private void buttonChageMasters_Click(object sender, RoutedEventArgs e)
         {
-
+            this.Frame.Navigate(new Pages.PageChangeMasters(Frame));
         }
 
         private void buttonChageClients_Click(object sender, RoutedEventArgs e)
