24b878fcda728778f620f9441c86ef8df45a1700
ConsoleC137 <artemcelipotkin@gmail.com>
2023-05-01 20:49:16 +0700

    Добавлен обработчик клика для кнопки 'Изменить администраторов'

---
+++ AutoServis/Pages/PageAdmin.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoServis.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAdmin.xaml
+    /// </summary>
+    public partial class PageAdmin : Page
+    {
+        private Frame Frame;
+        public PageAdmin(Frame frame)
+        {
+            InitializeComponent();
+            this.Frame = frame;
+        }
+
+        private void buttonChageAdmins_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(new Pages.PageChangeAdmins(Frame));
+        }
+
+        private void buttonChageMasters_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonChageClients_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonChageOrders_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
