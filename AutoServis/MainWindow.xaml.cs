c1130c7b8080209fc6bccf3b2e3f8386fda579e9
ConsoleC137 <artemcelipotkin@gmail.com>
2023-05-13 13:09:18 +0700

    Добавлен frame и код навигации.

--- AutoServis/MainWindow.xaml.cs
+++ AutoServis/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.SQLite;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace AutoServis
 {
@@ -23,6 +12,44 @@ namespace AutoServis
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Pages.PageAuth pageAuth = new Pages.PageAuth(mainFrame)
+            {
+                Width = Width,
+                Height = Height,
+                Title = "Авторизация"
+            };
+            mainFrame.Navigate(pageAuth);
+
+            //string connectionString = "Data Source=MyDatabase.sqlite;Version=3;";
+
+            //using (var connection = new SQLiteConnection(connectionString))
+            //{
+            //    connection.Open();
+
+            //    using (var command = new SQLiteCommand("CREATE TABLE admins (id INTEGER PRIMARY KEY AUTOINCREMENT, FIO TEXT NOT NULL, login TEXT UNIQUE NOT NULL, password TEXT UNIQUE NOT NULL)", connection))
+            //        command.ExecuteNonQuery();
+
+            //    string query = "INSERT INTO admins (FIO, login, password) VALUES ('Администратор по умолчанию', 'admin', 'admin')";
+            //    SQLiteCommand c = new SQLiteCommand(query, connection);
+            //    c.ExecuteNonQuery();
+
+            //    using (var command = new SQLiteCommand("CREATE TABLE masters (id INTEGER PRIMARY KEY AUTOINCREMENT, FIO TEXT NOT NULL, login TEXT UNIQUE NOT NULL, password TEXT UNIQUE NOT NULL)", connection))
+            //        command.ExecuteNonQuery();
+
+            //    using (var command = new SQLiteCommand("CREATE TABLE clients (id INTEGER PRIMARY KEY AUTOINCREMENT, FIO TEXT NOT NULL, car_id TEXT UNIQUE NOT NULL, phone TEXT UNIQUE NOT NULL)", connection))
+            //        command.ExecuteNonQuery();
+
+            //    using (var command = new SQLiteCommand("CREATE TABLE orders (id INTEGER PRIMARY KEY AUTOINCREMENT, client_id INTEGER NOT NULL, master_id INTEGER NOT NULL, start_date DATE NOT NULL, end_time DATE, price REAL NOT NULL, description TEXT NOT NULL, status TEXT NOT NULL)", connection))
+            //        command.ExecuteNonQuery();
+
+            //    using (var command = new SQLiteCommand("CREATE TABLE cars (id INTEGER PRIMARY KEY AUTOINCREMENT, VIN TEXT NOT NULL UNIQUE , mileage INTEGER NOT NULL, brand TEXT NOT NULL, model TEXT NOT NULL, year INTEGER NOT NULL)", connection))
+            //        command.ExecuteNonQuery();
+            //}
         }
     }
 }
